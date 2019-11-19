58de11c673b18d87c801dee25f510eb10d6c9851
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2019-11-19 15:21:49 +0100

    #Dockerize the service

--- write-in-elasticsearch/src/main/java/com/globalsupplychainthreatanalysis/dataprocessing/configuration/RabbitConfig.java
+++ write-in-elasticsearch/src/main/java/com/globalsupplychainthreatanalysis/dataprocessing/configuration/RabbitConfig.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,19 +15,21 @@ public class RabbitConfig {
     public static final String QUEUE = "events";
     public final static String TOPIC_EXCHANGE_NAME = "topic.exchange";
 
+    @Value(value = "rabbit.mq.routing-key")
+    private String routingKey;
+
     @Bean
     public Declarables topicBindings() {
         Queue topicQueue1 = new Queue(QUEUE, false);
 
         TopicExchange topicExchange = new TopicExchange(TOPIC_EXCHANGE_NAME, true, true);
-
         return new Declarables(
                 topicQueue1,
                 topicExchange,
                 BindingBuilder
                         .bind(topicQueue1)
                         //waiting for the rooting key name
-                        .to(topicExchange).with("*.important.*"));
+                        .to(topicExchange).with(routingKey));
     }
 
     @Bean
