0329f92396a2ee58dd429da8c3af4f4e07a69e29
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2019-11-29 22:16:26 +0100

    #34: implement endpoints for events search ,scroll and delete

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,10 +2,14 @@
 
 //Configuration variables
 const port      = process.env.PORT        || '3000';
-const elasticsearchHost = process.env.ELASTICSEARCH_HOST || '192.168.99.100';
+const elasticsearchHost = process.env.ELASTICSEARCH_HOST || '192.168.99.100:9200';
 const elasticsearchVersion = process.env.ELASTICSEARCH_Version || '7.4';
+const elasticsearchUsername = process.env.ELASTICSEARCH_USERNAME || 'elastic';
+const elasticsearchPassword = process.env.ELASTICSEARCH_PASSWORD || 'changeme';
 module.exports = {
     port,
     elasticsearchHost,
-    elasticsearchVersion
+    elasticsearchVersion,
+    elasticsearchUsername,
+    elasticsearchPassword
 };
