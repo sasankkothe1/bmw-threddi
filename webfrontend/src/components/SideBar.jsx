14f9351c7e3fb620e2a8bbb41efee44ec18d4d43
Sasank Kothe <ga53xil@mytum.de>
2019-12-01 22:17:21 +0100

    adjusted sidebar

--- webfrontend/src/components/SideBar.jsx
+++ webfrontend/src/components/SideBar.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 export default class SideBar extends Component {
     render () {
         return (
+            <div className="sidebarClass">
                 <aside className="aside aside-1">
                     <Link className="faIcon" to="/"><i className="fas fa-globe-americas"></i></Link>
                     <Link className="faIcon" to="/incidents"><i className="fas fa-bars"></i></Link>
                     <Link className="faIcon" to="/locations"><i className="fas fa-map-marker-alt"></i></Link>
                 </aside>
+            </div>
         )
     }
 }
