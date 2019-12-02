3791c39e3a01c1ebb885a6b5c4d4e681adb00062
Shun Long Hong <jira@shunlonghong.de>
2019-12-02 10:17:14 +0100

    Add form submission feature

--- webfrontend/src/components/FormContainer.jsx
+++ webfrontend/src/components/FormContainer.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 /* Import Components */
-import CheckBox from './components/CheckBox';
-import Input from './components/Input';
-import TextArea from './components/TextArea';
-import Select from './components/Select';
-import Button from './components/Button'
+import CheckBox from "./CheckBox";
+import Input from "./Input";
+import TextArea from "./TextArea";
+import Select from "./Select";
+import Button from "./Button";
 
 class FormContainer extends Component {
   constructor(props) {
@@ -13,23 +13,27 @@ class FormContainer extends Component {
 
     this.state = {
       newUser: {
-        name: '',
-        location_id: '',
-        lat: '',
-        long: '',
-        description: '',
-        priority: '',
-        type: ''
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: ""
       },
 
-      typeOptions: ['Production facility', 'Exhibition location', 'Others']
-
-    }
+      genderOptions: ["Male", "Female", "Others"],
+      skillOptions: ["Programming", "Development", "Design", "Testing"]
+    };
+    this.handleTextArea = this.handleTextArea.bind(this);
+    this.handleAge = this.handleAge.bind(this);
+    this.handleFullName = this.handleFullName.bind(this);
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.handleClearForm = this.handleClearForm.bind(this);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
-  /* This life cycle hook gets executed when the component mounts */
+  /* This lifecycle hook gets executed when the component mounts */
+
   handleFullName(e) {
     let value = e.target.value;
     this.setState(
@@ -70,27 +74,137 @@ class FormContainer extends Component {
     );
   }
 
-  handleFormSubmit() {
-    // Form submission logic
+  handleTextArea(e) {
+    console.log("Inside handleTextArea");
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          about: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
   }
-  handleClearForm() {
-    // Logic for resetting the form
+
+  handleCheckBox(e) {
+    const newSelection = e.target.value;
+    let newSelectionArray;
+
+    if (this.state.newUser.skills.indexOf(newSelection) > -1) {
+      newSelectionArray = this.state.newUser.skills.filter(
+        s => s !== newSelection
+      );
+    } else {
+      newSelectionArray = [...this.state.newUser.skills, newSelection];
+    }
+
+    this.setState(prevState => ({
+      newUser: { ...prevState.newUser, skills: newSelectionArray }
+    }));
   }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    let userData = this.state.newUser;
+
+    fetch("http://example.com", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Successful" + data);
+      });
+    });
+  }
+
+  handleClearForm(e) {
+    e.preventDefault();
+    this.setState({
+      newUser: {
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: ""
+      }
+    });
+  }
+
+
   render() {
     return (
-      <form className="container" onSubmit={this.handleFormSubmit}>
-
-        <Input /> {/* Name of the user */}
-        <Input /> {/* Input for Age */}
-        <Select /> {/* Gender Selection */}
-        <CheckBox /> {/* List of Skills (eg. Programmer, developer) */}
-        <TextArea /> {/* About you */}
-        <Button /> { /*Submit */ }
-        <Button /> {/* Clear the form */}
+      <form className="container-fluid" onSubmit={this.handleFormSubmit}>
+        <Input
+          inputType={"text"}
+          title={"Full Name"}
+          name={"name"}
+          value={this.state.newUser.name}
+          placeholder={"Enter your name"}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* Name of the user */}
+        <Input
+          inputType={"number"}
+          name={"age"}
+          title={"Age"}
+          value={this.state.newUser.age}
+          placeholder={"Enter your age"}
+          handleChange={this.handleAge}
+        />{" "}
+        {/* Age */}
+        <Select
+          title={"Gender"}
+          name={"gender"}
+          options={this.state.genderOptions}
+          value={this.state.newUser.gender}
+          placeholder={"Select Gender"}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* Age Selection */}
+        <CheckBox
+          title={"Skills"}
+          name={"skills"}
+          options={this.state.skillOptions}
+          selectedOptions={this.state.newUser.skills}
+          handleChange={this.handleCheckBox}
+        />{" "}
+        {/* Skill */}
+        <TextArea
+          title={"About you."}
+          rows={10}
+          value={this.state.newUser.about}
+          name={"currentPetInfo"}
+          handleChange={this.handleTextArea}
+          placeholder={"Describe your past experience and skills"}
+        />
+        {/* About you */}
+        <Button
+          action={this.handleFormSubmit}
+          type={"primary"}
+          title={"Submit"}
+          style={buttonStyle}
+        />{" "}
+        {/*Submit */}
+        <Button
+          action={this.handleClearForm}
+          type={"secondary"}
+          title={"Clear"}
+          style={buttonStyle}
+        />{" "}
+        {/* Clear the form */}
       </form>
     );
   }
 }
 
+const buttonStyle = {
+  margin: "10px 10px 10px 10px"
+};
+
 export default FormContainer;
-}
--- webfrontend/src/components/Incident.jsx
+++ webfrontend/src/components/Incident.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
- 
+import FormContainer from './FormContainer.jsx';
 
 export default class Incident extends Component {
     render () {
         return (
             <div className="main">
                 <h1> This is from Incident Page.</h1>
+                <h3> Sample Form Container </h3>
+                <FormContainer />
             </div>
         )
     }
