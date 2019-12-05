d772c61d97588a1d89821f474b38d3a7f2ad4ac2
Ḿarkus Böbel <markus.boebel@dxc.com>
2019-12-05 19:30:21 +0100

    Merge branch 'develop' into feature/create-incident-page
    # Conflicts:
    #	webfrontend/src/App.js
    #	webfrontend/src/components/Incident.jsx
    #	webfrontend/src/components/main.css

--- webfrontend/src/components/SideBar.jsx
+++ webfrontend/src/components/SideBar.jsx
@@ -2,22 +2,33 @@ import React, {Component} from 'react';
 import {withRouter} from "react-router";
 
 class SideBar extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.getActiveRouteElement = this.getActiveRouteElement.bind(this)
+    }
+
     render() {
         return (
             <div className="sidebarClass">
                 <aside className="aside">
-                    <div className={"nav-item active-route"} onClick={()=>this.forwardTo('/')}>
+                    <div className={"nav-item " + this.getActiveRouteElement('/')} onClick={()=>this.forwardTo('/')}>
                         <i className="fas fa-globe-americas item-nav fa-2x"/>
                     </div>
-                    <div className={"nav-item"} onClick={()=>this.forwardTo('/incidents')}>
+                    <div className={"nav-item " + this.getActiveRouteElement('/incidents')} onClick={()=>this.forwardTo('/incidents')}>
                         <i className="fas fa-bars fa-2x"/>
                     </div>
-                    <div className={"nav-item"} onClick={()=>this.forwardTo('/locations')}>
+                    <div className={"nav-item " + this.getActiveRouteElement('/locations')} onClick={()=>this.forwardTo('/locations')}>
                         <i className="fas fa-map-marker-alt fa-2x"/>
                     </div>
                 </aside>
             </div>
         )
+    }
+
+    getActiveRouteElement(path){
+        return path===this.props.location.pathname?"active-route":""
     }
 
     forwardTo(route){
