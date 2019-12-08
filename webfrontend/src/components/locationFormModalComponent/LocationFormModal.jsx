d97fdfebe8985c0bbfb2b6d6ce1c4fe605ad0283
Shun Long Hong <shunlong.hong@tum.de>
2019-12-08 11:21:31 +0100

    add location form to button as modal view

---
+++ webfrontend/src/components/locationFormModalComponent/LocationFormModal.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+import './LocationFormModal.css';
+import FormContainer from '../FormContainer'
+
+
+export default class LocationFormModal extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+    render() {
+        return (
+          <Modal
+                {...this.props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    Add location
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <div className="container">
+                    <FormContainer />
+                  </div>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant='danger' onClick={this.props.onHide}>Close</Button>
+                </Modal.Footer>
+              </Modal>
+        )
+    }
+}
