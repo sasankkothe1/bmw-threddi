4dd0d63d864a15f47b6e5e51afdfe5e2982a492d
Shun Long Hong <shunlong.hong@tum.de>
2019-12-09 18:25:13 +0100

    update file path structures, create folders for each component and the location view

--- webfrontend/src/components/locationFormModalComponent/LocationFormModal.jsx
+++ webfrontend/src/components/locationFormModalComponent/LocationFormModal.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
 import './LocationFormModal.css';
-import FormContainer from '../FormContainer'
+import FormContainer from '../locationFormComponent/FormContainer'
 
 
 export default class LocationFormModal extends Component {
