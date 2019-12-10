54e6b134c42f02e3acd67022d811dc54203bea2e
Ḿarkus Böbel <markus.boebel@dxc.com>
2019-12-10 11:07:57 +0100

    Add mock Extractor (GDELT DATA BASED)

--- webfrontend/src/config.js
+++ webfrontend/src/config.js
@@ -1,8 +1,9 @@
 
 let config = {
     GOOGLE_MAPS_API_KEY: "AIzaSyBFhcmedT0kXQ-hO1LrJszeQuePuqCq4vA",
-    BACKEND_URL: "localhost",
-    BACKEND_PORT: "4200"
+    // BACKEND_URL: "localhost",
+    BACKEND_URL: "192.168.99.100",
+    BACKEND_PORT: "3000"
 };
 
 export default config
