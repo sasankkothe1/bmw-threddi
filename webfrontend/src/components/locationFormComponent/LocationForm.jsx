400a0db06c039977de1187224bf5ee8e30ca7fe7
Shun Long Hong <shunlong.hong@tum.de>
2019-12-10 11:16:57 +0100

    message

---
+++ webfrontend/src/components/locationFormComponent/LocationForm.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import FormContainer from './FormContainer';
+
+export default class LocationForm extends Component {
+    render () {
+        return (
+          <div>
+          <h3> Sample Form Container </h3>
+          <FormContainer />
+          </div>
+        )
+    }
+}
---
+++ webfrontend/src/components/locationTableComponent/FilterableLocationTable.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import LocationTable from './LocationTable.jsx';
+import SearchBar from '../SearchBar';
+import Button from 'react-bootstrap/Button';
+import LocationFormModal from "../locationFormModalComponent/LocationFormModal";
+
+import "./FilterableLocationTable.css";
+
+// clear blocks to avoid mess up with other rows below
+
+export default class FilterableLocationTable extends Component {
+  constructor(props){
+    super(props);
+    this.state = {addModalShow : false}
+  }
+
+  render() {
+    let addModalClose =() => this.setState({addModalShow: false});
+
+    return (
+      <div className ="FilterableLocationTableDiv">
+        <div className="wrapper-search-bar">
+          <div className="search-bar">
+                <SearchBar id="filterText"/>
+          </div>
+
+          <div className="add-location-button">
+          <Button variant='success' onClick={()=> this.setState({addModalShow: true})}> Add location </Button>
+            <LocationFormModal
+              show={this.state.addModalShow}
+              onHide = {addModalClose} />
+          </div>
+        </div>
+
+        <LocationTable locations={this.props.locations} />
+      </div>)
+  }
+}
