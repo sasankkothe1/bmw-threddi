6b4c8840b72064685291323b2aa8cced731eac90
Shun Long Hong <shunlong.hong@tum.de>
2020-01-14 09:06:50 +0100

    deleted files that are not used anymore. styled login page.

--- webfrontend/src/views/login/LoginPage.jsx
+++ webfrontend/src/views/login/LoginPage.jsx
@@ -1,119 +1,54 @@
 import React, {Component} from 'react';
 import { Button} from "react-bootstrap";
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form'
 
-import { userActions } from '../../actions/user.actions';
 
 export default class LoginPage extends Component {
 
   constructor(props) {
-        super(props);
+    super(props);
 
-        // reset login status
-        this.props.logout();
+    this.state = {
+      email: '',
+    };
+  }
 
-        this.state = {
-            username: '',
-            password: '',
-            submitted: false
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
-        }
-    }
+  updateEmail(value) {
+    this.setState({
+      email: value,
+    });
+  }
 
     render () {
-      const { loggingIn } = this.props;
-      const { username, password, submitted } = this.state;
-
       return (
+        <div className="login-page-container">
+            <div className="login-form-container">
+              <Form className="login-form">
+                <Form.Label className="form-label">Log into Threddi</Form.Label>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control className="email-input-line" type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
 
-        <div className="login-container">
-        <div className="col-md-6 col-md-offset-3">
-            <h2>Login</h2>
-            <form name="form" onSubmit={this.handleSubmit}>
-                <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                    {submitted && !username &&
-                        <div className="help-block">Username is required</div>
-                    }
-                </div>
-                <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                    {submitted && !password &&
-                        <div className="help-block">Password is required</div>
-                    }
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-primary">Login</button>
-                    {loggingIn &&
-                        <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                    }
-                    //<Link to="/register" className="btn btn-link">Register</Link>
-                </div>
-            </form>
-        </div>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control className="password-input-line" type="password" placeholder="Password" />
+                </Form.Group>
 
-          <div className="login-header-container">
-            Header
-          </div>
-
-          <div className="email-address-elem-container">
-            Email address
-          </div>
-
-          <div className="password-elem-container">
-            Password
-          </div>
-
-          <div className="login-button-container">
-            <Button variant="secondary">Login</Button>
-          </div>
-
-          <div className="help-tool-bar-container">
-            <div className="create-account-button-container">
-              <Button variant="dark">Create account</Button>
+                  <Button className="login-button" variant="outline-light" type="submit">
+                    Login
+                  </Button>
+                </Form>
             </div>
 
-            <div className="recover-account-button-container">
-              <Button variant="dark">Recover account</Button>
+            <div className="help-tool-bar-container">
+              <Button variant="outline-light">Create account</Button>
+              <Button variant="outline-light">Recover account</Button>
+              <Button variant="outline-light">Help</Button>
             </div>
 
-            <div className="help-button-container">
-              <Button variant="dark">Help</Button>
-            </div>
           </div>
-        </div>
       )
     }
 }
-
-function mapState(state) {
-    const { loggingIn } = state.authentication;
-    return { loggingIn };
-}
-
-const actionCreators = {
-    login: userActions.login,
-    logout: userActions.logout
-};
-
-const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
-export { connectedLoginPage as LoginPage };
