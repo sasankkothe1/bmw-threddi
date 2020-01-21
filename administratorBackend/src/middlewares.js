50240eddf835563136461eed286e271b962b4880
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-01-21 16:34:59 +0100

    add the second authentication method

--- administratorBackend/src/middlewares.js
+++ administratorBackend/src/middlewares.js
@@ -42,6 +42,45 @@ const checkAuthentication = (req, res, next) => {
 
 };
 
+const checkAuthenticationForGetEndpoints = (req, res, next) => {
+
+    const authenticationType = req.headers['authentication_type'];
+    console.log(req.headers);
+    const authentication = req.headers['authentication'];
+    if (authenticationType && authenticationType === 'service') {
+        if (authentication === config.serviceAuthenticationCode) {
+            next();
+        } else {
+            return res.status(401).send({
+                error: 'Unauthorized',
+                message: 'Invalid Authentication Code.'
+            });
+        }
+
+    }
+    else {
+        if (!authentication || !authentication.includes('Bearer')) {
+            return res.status(401).send({
+                error: 'Unauthorized',
+                message: 'No token provided in the request'
+            });
+        }
+        const token = authentication.split('Bearer ')[1];
+        console.log('token = ' + token);
+        // verifies secret and checks exp
+        jwt.verify(token, config.jwtSecret, (err, decoded) => {
+            if (err) return res.status(401).send({
+                error: 'Unauthorized',
+                message: 'Failed to authenticate token.'
+            });
+
+            // if everything is good, save to request for use in other routes
+            req.userId = decoded.id;
+            next();
+        });
+    }
+};
+
 const errorHandler = (err, req, res, next) => {
     if (res.headersSent) {
         return next(err)
@@ -54,5 +93,6 @@ const errorHandler = (err, req, res, next) => {
 module.exports = {
     allowCrossDomain,
     checkAuthentication,
+    checkAuthenticationForGetEndpoints,
     errorHandler
 };
