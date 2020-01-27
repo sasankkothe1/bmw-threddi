e254ac11396b128ca2a4014df0d225d11bfae99f
Sasank Kothe <ga53xil@mytum.de>
2020-01-27 13:23:23 +0100

    made changes

--- webfrontend/src/components/configurationComponents/createConfigurationForm.jsx
+++ webfrontend/src/components/configurationComponents/createConfigurationForm.jsx
@@ -6,133 +6,71 @@ class CreateConfigurationForm extends Component {
     super(props);
 
     this.state = {
+      validated : false,
       newConfiguration: {
         config_name: "",
         config_id: "",
         config_processing: "",
         config_description: "",
-        field_mapping: {
-          original_fields: [],
-          mapping_fields: []
-        },
-        filter_options: {
-          field: [],
-          method: [],
-          threshold: []
-        },
-        source_dependent_information: {
-          name: [],
-          type: [],
-          description: []
-        }
+        original_fields: [],
+        mapping_fields: [],
+        filters: [],
+        methods: [],
+        thresholds: [],
+        sdi_names: [],
+        sdi_types: [],
+        sdi_descriptions: [],
       },
+      
       showError: false,
       error_msg: ""
     };
     
-    // this.addOriginalField = this.addMappingField.bind(this);
-    // this.addFilterField = this.addFilterField.bind(this);
-    // this.addSIIField = this.addSIIField.bind(this);
-    // this.handleConfigName = this.handleConfigName.bind(this);
-    // this.handleConfigProcessing = this.handleConfigProcessing.bind(this);
-    // this.handleConfigID = this.handleConfigID.bind(this);
-    // this.handleConfigDescription = this.handleConfigDescription.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.submitConfigurator = this.submitConfigurator.bind(this);
   }
 
   addMappingField() {
-      var parentOriginalFieldElement = document.getElementById("originalFieldColumn");
-      var parentMappingFieldelement = document.getElementById("mappingFieldColumn");
+      let parentOriginalFieldElement = document.getElementById("originalFieldColumn");
+      let parentMappingFieldelement = document.getElementById("mappingFieldColumn");
    
-    var newOriginalField = document.createElement("input");
-    var newMappingField = document.createElement("input");
-    newOriginalField.setAttribute("required",""); newOriginalField.setAttribute("placeholder","Original Field"); newOriginalField.setAttribute("type", "text"); newOriginalField.setAttribute("class","form-control");
-    newMappingField.setAttribute("required",""); newMappingField.setAttribute("placeholder","Mapping Field"); newMappingField.setAttribute("type", "text"); newMappingField.setAttribute("class","form-control");
+    let newOriginalField = document.createElement("input");
+    let newMappingField = document.createElement("input");
+    newOriginalField.setAttribute("required",""); newOriginalField.setAttribute("placeholder","Original Field"); newOriginalField.setAttribute("type", "text"); newOriginalField.setAttribute("class","form-control"); newOriginalField.setAttribute("name", "original_fields")
+    newMappingField.setAttribute("required",""); newMappingField.setAttribute("placeholder","Mapping Field"); newMappingField.setAttribute("type", "text"); newMappingField.setAttribute("class","form-control"); newMappingField.setAttribute("name", "mapping_fields")
     parentOriginalFieldElement.appendChild(newOriginalField);
     parentMappingFieldelement.appendChild(newMappingField);
   }
 
   addFilterField() {
-    var parentFilterFieldElement = document.getElementById("filterColumn");
-    var parentMethodFieldElement = document.getElementById("methodColumn");
-    var parentThresholdFieldElement = document.getElementById("thresholdColumn");
-  var newFilterField = document.createElement("input");
-  var newMethodField = document.createElement("input");
-  var newThresholdField = document.createElement("input");
-  newFilterField.setAttribute("required",""); newFilterField.setAttribute("placeholder","Filter"); newFilterField.setAttribute("type", "text"); newFilterField.setAttribute("class","form-control");
-  newMethodField.setAttribute("required",""); newMethodField.setAttribute("placeholder","Method"); newMethodField.setAttribute("type", "text"); newMethodField.setAttribute("class","form-control");
-  newThresholdField.setAttribute("required",""); newThresholdField.setAttribute("placeholder","Threshold"); newThresholdField.setAttribute("type", "text"); newThresholdField.setAttribute("class","form-control");
+    let parentFilterFieldElement = document.getElementById("filterColumn");
+    let parentMethodFieldElement = document.getElementById("methodColumn");
+    let parentThresholdFieldElement = document.getElementById("thresholdColumn");
+  let newFilterField = document.createElement("input");
+  let newMethodField = document.createElement("input");
+  let newThresholdField = document.createElement("input");
+  newFilterField.setAttribute("required",""); newFilterField.setAttribute("placeholder","Filter"); newFilterField.setAttribute("type", "text"); newFilterField.setAttribute("class","form-control"); newFilterField.setAttribute("name" , "filters");
+  newMethodField.setAttribute("required",""); newMethodField.setAttribute("placeholder","Method"); newMethodField.setAttribute("type", "text"); newMethodField.setAttribute("class","form-control"); newMethodField.setAttribute("name" , "methods")
+  newThresholdField.setAttribute("required",""); newThresholdField.setAttribute("placeholder","Threshold"); newThresholdField.setAttribute("type", "text"); newThresholdField.setAttribute("class","form-control"); newThresholdField.setAttribute("name" , "thresholds")
   parentFilterFieldElement.appendChild(newFilterField);
   parentMethodFieldElement.appendChild(newMethodField);
   parentThresholdFieldElement.appendChild(newThresholdField);
 }
 
 addSIIField(){
-    var parentSourceNameElement = document.getElementById("sourceName");
-    var parentSourceTypeElement = document.getElementById("sourceType");
-    var parentSourceDescriptionElement = document.getElementById("sourceDescription");
-  var newSourceNameField = document.createElement("input");
-  var newSourceTypeField = document.createElement("input");
-  var newSourceDescriptionField = document.createElement("input");
-  newSourceNameField.setAttribute("required",""); newSourceNameField.setAttribute("placeholder","Filter"); newSourceNameField.setAttribute("type", "text"); newSourceNameField.setAttribute("class","form-control");
-  newSourceTypeField.setAttribute("required",""); newSourceTypeField.setAttribute("placeholder","Method"); newSourceTypeField.setAttribute("type", "text"); newSourceTypeField.setAttribute("class","form-control");
-  newSourceDescriptionField.setAttribute("required",""); newSourceDescriptionField.setAttribute("placeholder","Threshold"); newSourceDescriptionField.setAttribute("type", "text"); newSourceDescriptionField.setAttribute("class","form-control");
+    let parentSourceNameElement = document.getElementById("sourceName");
+    let parentSourceTypeElement = document.getElementById("sourceType");
+    let parentSourceDescriptionElement = document.getElementById("sourceDescription");
+  let newSourceNameField = document.createElement("input");
+  let newSourceTypeField = document.createElement("input");
+  let newSourceDescriptionField = document.createElement("input");
+  newSourceNameField.setAttribute("required",""); newSourceNameField.setAttribute("placeholder","Filter"); newSourceNameField.setAttribute("type", "text"); newSourceNameField.setAttribute("class","form-control"); newSourceNameField.setAttribute("name" , "names")
+  newSourceTypeField.setAttribute("required",""); newSourceTypeField.setAttribute("placeholder","Method"); newSourceTypeField.setAttribute("type", "text"); newSourceTypeField.setAttribute("class","form-control"); newSourceTypeField.setAttribute("name" , "types")
+  newSourceDescriptionField.setAttribute("required",""); newSourceDescriptionField.setAttribute("placeholder","Threshold"); newSourceDescriptionField.setAttribute("type", "text"); newSourceDescriptionField.setAttribute("class","form-control"); newSourceDescriptionField.setAttribute("name", "descriptions")
   parentSourceNameElement.appendChild(newSourceNameField);
   parentSourceTypeElement.appendChild(newSourceTypeField);
   parentSourceDescriptionElement.appendChild(newSourceDescriptionField);
 }
-
-// handleConfigName = e => {
-//     let config_name = e.target.value;
-//         this.setState(
-//             prevState => ({
-//               newConfiguration: {
-//                     ...prevState.newConfiguration,
-//                     config_name: config_name
-//                 }
-//             }),
-//             () => console.log(this.state.newConfiguration.config_name)
-//         );
-//   };
-
-//   handleConfigProcessing = e => {
-//     let config_processing = e.target.value;
-//         this.setState(
-//             prevState => ({
-//               newConfiguration: {
-//                     ...prevState.newConfiguration,
-//                     config_processing: config_processing
-//                 }
-//             }),
-//             () => console.log(this.state.newConfiguration.config_processing)
-//         );
-//   };
-
-//   handleConfigID = e => {
-//     let config_id = e.target.value;
-//         this.setState(
-//             prevState => ({
-//               newConfiguration: {
-//                     ...prevState.newConfiguration,
-//                     config_id: config_id
-//                 }
-//             }),
-//             () => console.log(this.state.newConfiguration.config_id)
-//         );
-//   };
-
-//   handleConfigDescription = e => {
-//     let config_description = e.target.value;
-//         this.setState(
-//             prevState => ({
-//               newConfiguration: {
-//                     ...prevState.newConfiguration,
-//                     config_description: config_description
-//                 }
-//             }),
-//             () => console.log(this.state.newConfiguration.config_description)
-//         );
-//   };
 
   handleChange = e => {
       e.persist();
@@ -143,16 +81,121 @@ addSIIField(){
                 ...prevState.newConfiguration,
                 [e.target.name]: value
             }
-        }),
-        () => console.log(this.state.newConfiguration.config_id)
+        })//,
+        // ()  => console.log("config id : " + this.state.newConfiguration.config_id +
+        // "\n"+"config name : "+ this.state.newConfiguration.config_name + 
+        // "\n"+"config processing : "+ this.state.newConfiguration.config_processing + 
+        // "\n"+"config description : "+ this.state.newConfiguration.config_description)
     );
   }
 
+  // submitConfigurator(e){
+  //   e.persist();
+  //   e.preventDefault();
+  //   let org_fs= document.getElementsByName("original_fields");
+  //    for(let i = 0; i<org_fs.length; i++) {
+  //     let val = org_fs[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           original_fields: [...prevState.newConfiguration.original_fields, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   console.log(this.state.newConfiguration)
+  //   let maps_fs= document.getElementsByName("mapping_fields");
+  //   for(let i = 0; i<maps_fs.length; i++) {
+  //     let val = maps_fs[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           mapping_fields: [...prevState.newConfiguration.mapping_fields, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let fil_fs= document.getElementsByName("filters");
+  //   for(let i = 0; i<fil_fs.length; i++) {
+  //     let val = fil_fs[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           filters: [...prevState.newConfiguration.filters, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let mets= document.getElementsByName("methods");
+  //   for(let i = 0; i<mets.length; i++) {
+  //     let val = mets[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           methods: [...prevState.newConfiguration.methods, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let thres= document.getElementsByName("thresholds");
+  //   for(let i = 0; i<thres.length; i++) {
+  //     let val = thres[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           thresholds: [...prevState.newConfiguration.thresholds, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let sdi_name= document.getElementsByName("names");
+  //   for(let i = 0; i<sdi_name.length; i++) {
+  //     let val = sdi_name[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           sdi_names: [...prevState.newConfiguration.sdi_names, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let sdi_type= document.getElementsByName("types");
+  //   for(let i = 0; i<sdi_type.length; i++) {
+  //     let val = sdi_type[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           sdi_types: [...prevState.newConfiguration.sdi_types, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   let sdi_des= document.getElementsByName("descriptions");
+  //   for(let i = 0; i<sdi_des.length; i++) {
+  //     let val = sdi_des[i].value;
+  //     console.log(val);
+  //     this.setState (
+  //       prevState => ({
+  //         newConfiguration: {
+  //           sdi_descriptions: [...prevState.newConfiguration.sdi_descriptions, val]
+  //         }
+  //       })
+  //     );
+  //   }
+  //   console.log(this.state.newConfiguration);
+  // }
 
 
   render() {
     return (
-      <Form>
+      <Form noValidate validated={this.state.validated}>
         <Form.Group as={Row} md="6">
           <Form.Label column sm={2}>
             Name
@@ -197,12 +240,12 @@ addSIIField(){
         <Form.Row>
           <Form.Group as={Col} md="6" id="originalFieldColumn">
             <Form.Label>Orginal Field</Form.Label>
-            <Form.Control required type="text" placeholder="Original Field" />
+            <Form.Control required type="text" placeholder="Original Field" name = "original_fields" />
           </Form.Group>
 
           <Form.Group as={Col} md="6" id="mappingFieldColumn">
             <Form.Label>Mapping Field</Form.Label>
-            <Form.Control required type="text" placeholder="Mapping Field" />
+            <Form.Control required type="text" placeholder="Mapping Field" name = "mapping_fields" />
           </Form.Group>
           <Button block variant="light" onClick={this.addMappingField}>
             + Original and Mapping
@@ -212,17 +255,17 @@ addSIIField(){
         <Form.Row>
           <Form.Group as={Col} md="4" id="filterColumn">
             <Form.Label>Filter</Form.Label>
-            <Form.Control required type="text" placeholder="Filter" />
+            <Form.Control required type="text" placeholder="Filter" name = "filters" />
           </Form.Group>
 
           <Form.Group as={Col} md="4" id="methodColumn">
             <Form.Label>Method</Form.Label>
-            <Form.Control required type="text" placeholder="Method" />
+            <Form.Control required type="text" placeholder="Method" name = "methods" />
           </Form.Group>
 
           <Form.Group as={Col} md="4" id="thresholdColumn">
             <Form.Label>Threshold</Form.Label>
-            <Form.Control required type="text" placeholder="Threshold" />
+            <Form.Control required type="text" placeholder="Threshold" name = "thresholds"/>
           </Form.Group>
           <Button block variant="light" onClick={this.addFilterField}>
             + Filter
@@ -232,21 +275,25 @@ addSIIField(){
         <Form.Row>
           <Form.Group as={Col} md="4" id="sourceName">
             <Form.Label>Name</Form.Label>
-            <Form.Control required type="text" placeholder="Name" />
+            <Form.Control required type="text" placeholder="Name" name = "names" />
           </Form.Group>
 
           <Form.Group as={Col} md="4" id="sourceType">
             <Form.Label>Type</Form.Label>
-            <Form.Control required type="text" placeholder="Type" />
+            <Form.Control required type="text" placeholder="Type" name = "types"/>
           </Form.Group>
 
           <Form.Group as={Col} md="4" id="sourceDescription">
             <Form.Label>Description</Form.Label>
-            <Form.Control required type="text" placeholder="Description" />
+            <Form.Control required type="text" placeholder="Description" name = "descriptions" />
           </Form.Group>
           <Button block variant="light" onClick={this.addSIIField}>
             + Source Dependant Information
           </Button>
+        </Form.Row>
+        <hr/>
+        <Form.Row>
+          <Button block type="submit" onClick = {this.submitConfigurator}>Add the configurator</Button>
         </Form.Row>
       </Form>
     );
