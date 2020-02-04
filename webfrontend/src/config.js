c02c279f1be22ef1fbb3c58d05bf87cf57561cf2
Xiaolin Ma <xiaolin.ma@ic-consult.com>
2020-02-04 09:59:49 +0100

    change return object to json

--- webfrontend/src/config.js
+++ webfrontend/src/config.js
@@ -1,12 +1,12 @@
 
 let config = {
     GOOGLE_MAPS_API_KEY: "AIzaSyBFhcmedT0kXQ-hO1LrJszeQuePuqCq4vA",
-     BACKEND_URL: "localhost",
-    //BACKEND_URL: "192.168.99.100",
+    //BACKEND_URL: "localhost",
+    BACKEND_URL: "192.168.99.100",
     BACKEND_PORT: "4200",
     //ADMIN_BACKEND_URL: "administrator-backend",
-    ADMIN_BACKEND_URL: "localhost",
-    //ADMIN_BACKEND_URL: "192.168.99.100",
+    //ADMIN_BACKEND_URL: "localhost",
+    ADMIN_BACKEND_URL: "192.168.99.100",
     ADMIN_BACKEND_PORT: "4300",
     EXTRACTOR_URL: "localhost",
     EXTRACTOR_PORT: 4321
